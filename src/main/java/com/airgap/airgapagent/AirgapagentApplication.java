e50b3d0193eab335540c2c232b7a259e7c1e58f1
Jacques Fontignie <jacques.fontignie@gmail.com>
2020-05-07 08:22:23 +0200

    ignore warning

--- src/main/java/com/airgap/airgapagent/AirgapagentApplication.java
+++ src/main/java/com/airgap/airgapagent/AirgapagentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AirgapagentApplication {
 
+    @SuppressWarnings("java:S4823")
     public static void main(String[] args) {
         SpringApplication.run(AirgapagentApplication.class, args);
     }
